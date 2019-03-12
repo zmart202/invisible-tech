2583521661daa17a16f0b78e840c1568e0e7f15e
zmartin-ezcater <zachary.martin@ezcater.com>
2019-03-11 22:04:56 -0600

    created a tool that gives you weather and local time based on postal code and location name

---
+++ weather.js
@@ -0,0 +1,44 @@
+const axios = require('axios');
+
+const locationArray = [
+    {
+        locationName: 'us',
+        postalCode: '01760'
+    },
+    {
+        locationName: 'us',
+        postalCode: '84111'
+    }
+]
+
+async function getWeather(){
+    try {
+        for (let loc in locationArray) {
+            const location = locationArray[loc]
+
+            // Pulling from OpenWeatherMap API to get the weather and coordinates of location
+            const weather = 
+                await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${location.postalCode},${location.locationName}&APPID=ed8d80476c16466b123b5d24ad720a6a`)
+            
+            // Pulling from DarkSky using coordinates to get exact time based on timezone
+            const coords = 
+                await axios.get(`https://api.darksky.net/forecast/842ac45abba7d10d96a6983d990e63d4/${weather.data.coord.lat},${weather.data.coord.lon}`)
+            
+            const options = {
+                timeZone: `${coords.data.timezone}`,
+                year: 'numeric', month: 'numeric', day: 'numeric',
+                hour: 'numeric', minute: 'numeric', second: 'numeric',
+            }
+
+            // Converting the current time based on the timezone using the options above
+            const formatter = new Intl.DateTimeFormat([], options)
+            const formattedTime = formatter.format(new Date())
+
+            console.log('Weather: ', weather.data.main,'\n', 'Current Time: ', formattedTime)
+        }
+    } catch(e) {
+        console.error(e)
+    }
+}
+
+getWeather()
